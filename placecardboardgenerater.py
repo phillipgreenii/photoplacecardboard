723619e961ce31101b5c87e178d56dfe247ec145
Phillip Green II <deathcheese@yahoo.com>
2011-06-13 23:59:56 -0400

    crop images into correctly sized parts

--- placecardboardgenerater.py
+++ placecardboardgenerater.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from area import Area
-
+import Image
 
 verticalCardsCount = 15
 horizontalCardsCount = 15
@@ -14,11 +14,14 @@ cardHeight = 2
 cardWidth = 3
 spaceBetweenCards = 0.125
 
-pictureHeight = 2400
-pictureWidth = 3600
+im = Image.open("picture.jpg")
+
+pictureHeight = im.size[1]
+pictureWidth = im.size[0]
 
 #(0,0) is upper left corner of picture
-unusedArea = Area( (845, 1600) , 800, 400)
+unusedArea = Area( (765, 1610) , 740, 330)
+
 print "unusedArea: %s " % unusedArea
 
 
@@ -35,7 +38,16 @@ for i in range(verticalCardsCount):
         cards.append(Area(point, cardHeightPixels, cardWidthPixels))
 print len(cards)
 
+
 printableCards = filter(lambda a: not a.intersects(unusedArea), cards)
 print len(printableCards)
 
+ignoredCards =  filter(lambda a: a.intersects(unusedArea), cards)
 
+unusedAreaPicture = im.crop((unusedArea.upperLeftPoint[0],unusedArea.upperRightPoint[1], unusedArea.lowerRightPoint[0], unusedArea.lowerRightPoint[1]))
+unusedAreaPicture.save("tmp/nocardarea.jpg", "JPEG")
+
+for (i,card) in enumerate(printableCards):
+    box = (card.upperLeftPoint[0],card.upperRightPoint[1], card.lowerRightPoint[0], card.lowerRightPoint[1])
+    part = im.crop(box)
+    part.save("tmp/part%04i.jpg" % i, "JPEG")
